490c21f3552f2b215ea3512bf8ecaf6a4ebbd48a
Athes <168402508+Atheswar@users.noreply.github.com>
2024-07-23 22:26:49 +0530

    Create Sorting Array of Strings.c

---
+++ 23.07.2024/Sorting Array of Strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void sortStrings(char arr[][100], int n) {
+    char temp[100];
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (strcmp(arr[i], arr[j]) > 0) {
+                strcpy(temp, arr[i]);
+                strcpy(arr[i], arr[j]);
+                strcpy(arr[j], temp);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    char arr[n][100];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    sortStrings(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
+
+    return 0;
+}
