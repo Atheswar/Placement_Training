0da6c177b3ceeec7c547c704ab3cc4c2d55d8b17
Athes <168402508+Atheswar@users.noreply.github.com>
2024-07-23 22:20:07 +0530

    Update and rename Time Delta.py to Java Anagrams.java

---
+++ 23.07.2024/Java Anagrams.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        
+        int[] frequency = new int[26];
+        
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        for (int i = 0; i < a.length(); i++) {
+            frequency[a.charAt(i) - 'a']++;
+            frequency[b.charAt(i) - 'a']--;
+        }
+        
+        for (int count : frequency) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+    }
+}
